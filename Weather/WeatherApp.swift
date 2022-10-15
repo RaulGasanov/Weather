3873a5bd0fbc99da1e5e9b68473da717baaf9cfa
Raul_Gasanov <raulfutbollist@gmail.com>
2022-10-15 13:22:20 +0300

    Initial Commit

---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by Raul_Gasanov on 15.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
